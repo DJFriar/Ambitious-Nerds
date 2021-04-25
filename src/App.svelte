a3279d203080978a5085a1d40ad3744094608a62
DJFriar <tommy.craft@icloud.com>
2021-04-25 17:45:45 -0500

    Initial setup

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  import Header from './components/Header.svelte';
+  import Footer from './components/Footer.svelte';
+	export let name;
+</script>
+
+<Header />
+
+<main>
+	<p>We are nerds. We are ambitious. We are...</p>
+	<h1>Ambitous Nerds, LLC</h1>
+  <p>We build software applications and web sites.</p>
+</main>
+
+<Footer />
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #863084;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,23 @@
+<header>
+  <p>Logo coming soon... submit your own design idea at <a href="http://99d.me/e/n7fe">99designs.com</a></p>
+  <!-- <h1>
+    <img src="/img/poll_ninja_logo.svg" alt="Poll Ninja Logo">
+  </h1> -->
+</header>
+
+<style>
+  header {
+    /* background: #f7f7f7; */
+    background: #27313D;
+    padding: 20px;
+    border-bottom: 2px solid #863084;
+  }
+
+  h1 {
+    margin: 0;
+    text-align: center;
+  }
+  img {
+    max-width: 180px;
+  }
+</style>
